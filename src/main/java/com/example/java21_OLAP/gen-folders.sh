40b3c012adf57357931bd3939122d58d488163af
Waltor <longmengua@users.noreply.github.com>
2025-09-28 05:50:37 +0800

    init

---
+++ src/main/java/com/example/java21_OLAP/gen-folders.sh
@@ -0,0 +1,17 @@
+mkdir -p ./application/command \
+ ./application/event/handlers \
+ ./application/scheduler \
+ ./application/service \
+ ./application/usecase \
+ ./domain/event \
+ ./domain/model \
+ ./domain/repository \
+ ./infrastructure/config \
+ ./infrastructure/kafka \
+ ./infrastructure/redis \
+ ./interfaces/consumer \
+ ./interfaces/web/controller \
+ ./interfaces/web/dto \
+ ./interfaces/web/exception \
+ ./interfaces/web/interceptor \
+ ./interfaces/web/validator
