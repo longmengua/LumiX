6e53c93042339bdd5889dde56c6739b052beca5a
Waltor <longmengua@users.noreply.github.com>
2025-09-28 05:10:02 +0800

    feat: init

---
+++ src/main/java/com/example/java21_OLAP/Java21OlapApplication.java
@@ -0,0 +1,13 @@
+package com.example.java21_OLAP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java21OlapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java21OlapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/java21_OLAP/Java21OlapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java21_OLAP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java21OlapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
